6464a71f3f945fa98e248887333edcc9e2def18f
Quang <20378quang@gmail.com>
2026-09-04 15:23:06 -0400

    swift init

---
+++ DashPilot/DashPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashPilotApp.swift
+//  DashPilot
+//
+//  Created by q on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DashPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
